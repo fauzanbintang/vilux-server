3a17717dfabc12c91bf34abf92a2ed29f802c89c
fauzanbintang <obin.aji@gmail.com>
2024-10-21 23:11:32 +0700

    fix: voucher discount notif

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -4,7 +4,7 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { CreateOrderDto, UpdateOrderDto } from 'src/dto/request/order.dto';
 import { generateCode } from 'src/helpers/order_code_generator';
 import { UserDto } from 'src/dto/response/user.dto';
-import { LegitCheckStatus, Role } from '@prisma/client';
+import { LegitCheckStatus, Prisma, Role } from '@prisma/client';
 import { ServiceDto } from 'src/dto/response/service.dto';
 import { VoucherDto } from 'src/dto/response/voucher.dto';
 
@@ -17,53 +17,62 @@ export class OrderService {
   async create(clientInfo: UserDto, createOrderDto: CreateOrderDto) {
     this.logger.debug(`Create order ${JSON.stringify(createOrderDto)}`);
 
-    let service = await this.prismaService.services.findUnique({
-      where: { id: createOrderDto.service_id },
-    });
-
-    let newService: ServiceDto = {
-      ...service,
-      normal_price: service.normal_price.toString(),
-      vip_price: service.vip_price.toString(),
-    };
-
-    // TODO: add validation for refund (one time only)
-    // TODO: add validation for one user one time only
-    let voucher: VoucherDto;
-    if (createOrderDto.voucher_id && createOrderDto.voucher_id.length > 0) {
-      voucher = await this.prismaService.voucher.findUnique({
-        where: { id: createOrderDto.voucher_id },
+    try {
+      let service = await this.prismaService.services.findUnique({
+        where: { id: createOrderDto.service_id },
       });
 
-      const now = new Date(Date.now());
-      if (now < voucher.started_at || now > voucher.expired_at) {
-        throw new HttpException('Voucher not valid', 400);
+      let newService: ServiceDto = {
+        ...service,
+        normal_price: service.normal_price.toString(),
+        vip_price: service.vip_price.toString(),
+      };
+
+      // TODO: add validation for refund (one time only)
+      // TODO: add validation for one user one time only
+      let voucher: VoucherDto;
+      if (createOrderDto.voucher_id && createOrderDto.voucher_id.length > 0) {
+        voucher = await this.prismaService.voucher.findUnique({
+          where: { id: createOrderDto.voucher_id },
+        });
+
+        const now = new Date(Date.now());
+        if (now < voucher.started_at || now > voucher.expired_at) {
+          throw new HttpException('Voucher not valid', 400);
+        }
       }
+
+      const original_amount = countTotalOriginalAmount(
+        clientInfo.role,
+        newService,
+      );
+
+      const order = await this.prismaService.order.create({
+        data: {
+          code: generateCode('OD'),
+          legit_check_id: createOrderDto.legit_check_id,
+          service_id: createOrderDto.service_id,
+          original_amount: original_amount,
+          voucher_id: createOrderDto.voucher_id,
+        },
+      });
+
+      await this.prismaService.legitChecks.update({
+        where: { id: createOrderDto.legit_check_id },
+        data: {
+          check_status: LegitCheckStatus.payment,
+        },
+      });
+
+      return { ...order, original_amount: order.original_amount.toString() };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new HttpException("order already created", 400);
+        }
+      }
+      throw new HttpException(error.message, 500);
     }
-
-    const original_amount = countTotalOriginalAmount(
-      clientInfo.role,
-      newService,
-    );
-
-    const order = await this.prismaService.order.create({
-      data: {
-        code: generateCode('OD'),
-        legit_check_id: createOrderDto.legit_check_id,
-        service_id: createOrderDto.service_id,
-        original_amount: original_amount,
-        voucher_id: createOrderDto.voucher_id,
-      },
-    });
-
-    await this.prismaService.legitChecks.update({
-      where: { id: createOrderDto.legit_check_id },
-      data: {
-        check_status: LegitCheckStatus.payment,
-      },
-    });
-
-    return { ...order, original_amount: order.original_amount.toString() };
   }
 
   async findAll() {
