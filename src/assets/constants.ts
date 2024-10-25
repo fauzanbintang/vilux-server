afa129be28b0b0a86db82a79f378ac416df63ce0
fauzanbintang <obin.aji@gmail.com>
2024-10-25 19:54:05 +0700

    feat: add notification to success payment

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,8 +1,47 @@
 export const LedgerConst = {
-  LegitCheckService: "Legit Check Service",
-  PaymentGatewayFee: "Payment Gateway Fee",
-  Order: "orders",
-  Margin: "vilux margin",
-  Voucher: "Voucher Discount",
-  ViluxMargin: "Vilux Margin"
+  LegitCheckService: 'Legit Check Service',
+  PaymentGatewayFee: 'Payment Gateway Fee',
+  Order: 'orders',
+  Margin: 'vilux margin',
+  Voucher: 'Voucher Discount',
+  ViluxMargin: 'Vilux Margin',
 } as const;
+
+export const NotificationConst = {
+  SuccessVerifyEmail: {
+    title: 'Welcome to Vilux',
+    body: "Start verifying your luxury items now, quick and easy. We're here to ensure you only have the real deal. Let's get started!",
+  },
+  PendingPaymentUser: {
+    title: 'Your have pending payment [order] from Vilux',
+    body: 'Pending Payment Alert! You have 60 mins to complete your payment, complete payment now to continue with your legit check',
+  },
+  SuccessPaymentUser: {
+    title: 'Thank you for submitting your item',
+    body: "Your item has been successfully submitted. We'll begin the legit check process and notify you once it's completed. Stay tuned!",
+  },
+  SuccessPaymentAdmin: {
+    title: 'Pending Order Alert - [User Role]',
+    body: 'You have a new legit-check request for Order [Order ID]. Time to review and process!',
+  },
+  RejectedDataValidation: {
+    title: 'We cannot process your item',
+    body: 'We encountered an issue with your submitted data and cannot process your item. Please review your submission and response to our notes.',
+  },
+  RevisedData: {
+    title: 'Order [Order ID] - User Responsed',
+    body: 'You have rejected data to response',
+  },
+  ApprovedDataValidation: {
+    title: 'Your Item is Now Being Checked!',
+    body: "We've started the legit check process for your item. Sit tight, and we'll update you with the results soon!",
+  },
+  DoneLegitCheck: {
+    title: 'Your item has been completely checked',
+    body: "We've finished check your items, please preview your certificate result on apps. Thank you for using our service!",
+  },
+  UnidentifiedLegitCheck: {
+    title: 'Sorry we fail to check your item',
+    body: "We couldn’t complete your item check. Don't worry, we've sent you a refund voucher for your next check. Check your app for details!",
+  },
+};
